e3f0a177c5b1180cb31463eded4832e58c563ebd
Alexey-t <support@uvviewsoft.com>
2018-04-12 20:27:19 +0300

    add demo_statusbar_hints

---
+++ app/demo_statusbar_hints/unit1.pas
@@ -0,0 +1,30 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ATStatusBar;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    ATStatus1: TATStatus;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
