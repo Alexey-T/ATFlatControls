fd21a358f57c209e5d9e7af2bdbf16d3254e930a
srd-software <51385541+srd-software@users.noreply.github.com>
2019-06-24 10:22:25 -0600

    Fix and update demo
    Fix and update demo

--- app/demo_scrollbar_delphi/DemoForm.pas
+++ app/demo_scrollbar_delphi/DemoForm.pas
@@ -28,6 +28,8 @@ type
     Label5: TLabel;
     trackCornerV: TTrackBar;
     trackCornerH: TTrackBar;
+    ListArrows: TListBox;
+    chkInstant: TCheckBox;
     procedure FormCreate(Sender: TObject);
     procedure chkDrawClick(Sender: TObject);
     procedure trackBorChange(Sender: TObject);
@@ -36,15 +38,16 @@ type
     procedure trackLongerChange(Sender: TObject);
     procedure trackCornerVChange(Sender: TObject);
     procedure trackCornerHChange(Sender: TObject);
+    procedure ListArrowsClick(Sender: TObject);
   private
     { Private declarations }
     procedure ChangeH(S: TObject);
     procedure ChangeV(S: TObject);
-    procedure DrawEvent(S: TObject; AType: TATScrollElemType;
+    procedure DrawEvent(S: TObject; AType: TATScrollbarElemType;
       ACanvas: TCanvas; const ARect: TRect; var ACanDo: boolean);
   public
     { Public declarations }
-    bh, bv, bbv, bbh: TATScroll;
+    bar_h, bar_v, bar_h1, bar_v1: TATScrollbar;
   end;
 
 var
@@ -58,96 +61,137 @@ uses StrUtils, Math;
 
 procedure TFormDemo.FormCreate(Sender: TObject);
 begin
-  bh:= TATScroll.Create(Self);
-  bh.Parent:= Panel1;
-  bh.Align:= alBottom;
-  bh.Kind:= sbHorizontal;
-  bh.OnChange:= {$ifdef fpc}@{$endif} ChangeH;
-  bh.Min:= 20;
-  bh.Max:= 200;
+  bar_h:= TATScrollbar.Create(Self);
+  bar_h.Parent:= Panel1;
+  bar_h.Align:= alBottom;
+  bar_h.Kind:= sbHorizontal;
+  bar_h.OnChange:= ChangeH;
+  bar_h.Min:= 20;
+  bar_h.Max:= 200;
 
   //-----------------------------------
-  bv:= TATScroll.Create(Self);
-  bv.Parent:= Panel1;
-  bv.Align:= alRight;
-  bv.Kind:= sbVertical;
-  bv.OnChange:= {$ifdef fpc}@{$endif} ChangeV;
-  bv.Min:= 10;
-  bv.Max:= 100;
+  bar_v:= TATScrollbar.Create(Self);
+  bar_v.Parent:= Panel1;
+  bar_v.Align:= alRight;
+  bar_v.Kind:= sbVertical;
+  bar_v.OnChange:= ChangeV;
+  bar_v.Min:= 10;
+  bar_v.Max:= 100;
 
-  bv.Width:= 22;
-  bh.Height:= bv.Width;
-  bh.IndentCorner:= bv.Width; //positive
+  bar_v.Width:= ATScrollbarTheme.InitialSize;
+  bar_h.Height:= ATScrollbarTheme.InitialSize;
+  bar_h.IndentCorner:= 100;
 
   //--------------
-  bbh:= TATScroll.Create(Self);
-  bbh.Parent:= Panel2;
-  bbh.Height:= 18;
-  bbh.Align:= alBottom;
-  bbh.Kind:= sbHorizontal;
-  bbh.IndentCorner:= -bbh.Height; //negative
+  bar_h1:= TATScrollbar.Create(Self);
+  bar_h1.Parent:= Panel2;
+  bar_h1.Height:= ATScrollbarTheme.InitialSize;
+  bar_h1.Align:= alBottom;
+  bar_h1.Kind:= sbHorizontal;
+  bar_h1.IndentCorner:= -100; //negative
 
-  bbv:= TATScroll.Create(Self);
-  bbv.Parent:= Panel2;
-  bbv.Width:= bbh.Height;
-  bbv.Align:= alLeft;
-  bbv.Kind:= sbVertical;
+  bar_v1:= TATScrollbar.Create(Self);
+  bar_v1.Parent:= Panel2;
+  bar_v1.Width:= ATScrollbarTheme.InitialSize;
+  bar_v1.Align:= alLeft;
+  bar_v1.Kind:= sbVertical;
 end;
 
-procedure TFormDemo.DrawEvent(S: TObject; AType: TATScrollElemType;
+procedure TFormDemo.ListArrowsClick(Sender: TObject);
+begin
+  if ListArrows.ItemIndex<=Ord(High(TATScrollbarArrowsStyle)) then
+  begin
+    ATScrollbarTheme.ArrowStyleH:= TATScrollbarArrowsStyle(ListArrows.ItemIndex);
+    ATScrollbarTheme.ArrowStyleV:= ATScrollbarTheme.ArrowStyleH;
+  end
+  else
+  begin
+    ATScrollbarTheme.ArrowStyleH:= asaArrowsAbove;
+    ATScrollbarTheme.ArrowStyleV:= asaArrowsBelow;
+  end;
+
+  bar_v.Invalidate;
+  bar_h.Invalidate;
+  bar_v1.Invalidate;
+  bar_h1.Invalidate;
+
+end;
+
+procedure TFormDemo.DrawEvent(S: TObject; AType: TATScrollbarElemType;
   ACanvas: TCanvas; const ARect: TRect; var ACanDo: boolean);
 const
-  cc: array[TATScrollElemType] of TColor = (
+  cc: array[TATScrollbarElemType] of TColor = (
     clYellow, clYellow,
     clCream, clCream,
     $30a030, $00e000, 
     $e0a0c0, clNavy,
     clRed div 2, clRed,
     $e05050);
+var
+  str: string;
+  p: TPoint;
 begin
   ACanvas.Brush.Color:= cc[AType];
   ACanvas.FillRect(ARect);
   ACanDo:= false;
+
+  case AType of
+    aseArrowUp: str:= '^';
+    aseArrowDown: str:= 'v';
+    aseArrowLeft: str:= '<';
+    aseArrowRight: str:= '>';
+    aseScrollThumbH: str:= '==';
+    aseScrollThumbV: str:= '||';
+    else str:= '';
+  end;
+
+  if str<>'' then
+  begin
+    p.x:= (ARect.Left+ARect.Right-ACanvas.TextWidth(str)) div 2;
+    p.y:= (ARect.Top+ARect.Bottom-ACanvas.TextHeight(str)) div 2;
+    ACanvas.TextOut(p.x, p.y, str);
+  end;
 end;
 
 procedure TFormDemo.chkDrawClick(Sender: TObject);
 begin
   if chkDraw.Checked then
   begin
-    bh.OnOwnerDraw:= {$ifdef fpc}@{$endif} DrawEvent;
-    bv.OnOwnerDraw:= {$ifdef fpc}@{$endif} DrawEvent;
+    bar_h.OnOwnerDraw:= DrawEvent;
+    bar_v.OnOwnerDraw:= DrawEvent;
   end
   else
   begin
-    bh.OnOwnerDraw:= nil;
-    bv.OnOwnerDraw:= nil;
+    bar_h.OnOwnerDraw:= nil;
+    bar_v.OnOwnerDraw:= nil;
   end;
-  bh.Invalidate;
-  bv.Invalidate;
+  bar_h.Invalidate;
+  bar_v.Invalidate;
 end;
 
 procedure TFormDemo.trackBorChange(Sender: TObject);
 begin
-  bv.IndentBorder:= trackBor.Position;
-  bh.IndentBorder:= bv.IndentBorder;
-  bv.Invalidate;
-  bh.Invalidate;
+  ATScrollbarTheme.BorderSize:= trackBor.Position;
+  bar_v.Invalidate;
+  bar_h.Invalidate;
+  bar_v1.Invalidate;
+  bar_h1.Invalidate;
 end;
 
 procedure TFormDemo.ChangeH(S: TObject);
 begin
-  labh.Caption:= Format('Horz %d (%d .. %d)', [bh.Position, bh.Min, bh.Max]);
+  labh.Caption:= Format('Horz %d (%d .. %d)', [bar_h.Position, bar_h.Min, bar_h.Max]);
 end;
 
 procedure TFormDemo.ChangeV(S: TObject);
 begin
-  labv.Caption:= Format('Vert %d (%d .. %d)', [bv.Position, bv.Min, bv.Max]);
+  labv.Caption:= Format('Vert %d (%d .. %d)', [bar_v.Position, bar_v.Min, bar_v.Max]);
 end;
 
 procedure TFormDemo.trackPageChange(Sender: TObject);
 begin
-  bv.PageSize:= trackPage.Position;
-  bh.PageSize:= bv.PageSize;
+  bar_v.PageSize:= trackPage.Position;
+  bar_h.PageSize:= bar_v.PageSize;
 end;
 
 procedure TFormDemo.trackSizeChange(Sender: TObject);
@@ -155,31 +199,32 @@ var
   n: Integer;
 begin
   n:= trackSize.Position;
-  bv.IndentArrow:= n div 5;
-  bh.IndentArrow:= bv.IndentArrow;
-  bv.Width:= n;
-  bh.IndentCorner:= n;
-  bh.Height:= n;
+  ATScrollbarTheme.ScalePercents:= n;
+  bar_v.Update;
+  bar_h.Update;
+  bar_v1.Update;
+  bar_h1.Update;
 end;
 
 procedure TFormDemo.trackLongerChange(Sender: TObject);
 begin
-  bh.IndentArrLonger:= trackLonger.Position;
-  bv.IndentArrLonger:= trackLonger.Position;
-  bh.Invalidate;
-  bv.Invalidate;
+  ATScrollbarTheme.ArrowLengthPercents:= trackLonger.Position;
+  bar_h.Invalidate;
+  bar_v.Invalidate;
+  bar_h1.Invalidate;
+  bar_v1.Invalidate;
 end;
 
 procedure TFormDemo.trackCornerVChange(Sender: TObject);
 begin
-  bv.IndentCorner:= trackCornerV.Position;
-  bv.Invalidate;
+  bar_v.IndentCorner:= trackCornerV.Position;
+  bar_v.Invalidate;
 end;
 
 procedure TFormDemo.trackCornerHChange(Sender: TObject);
 begin
-  bh.IndentCorner:= trackCornerH.Position;
-  bh.Invalidate;
+  bar_h.IndentCorner:= trackCornerH.Position;
+  bar_h.Invalidate;
 end;
 
 end.
