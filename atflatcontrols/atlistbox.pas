b4c6df08ddf87b776f0597c5dc94c6e5dfe046f1
Alexey <support@uvviewsoft.com>
2021-04-28 20:31:46 +0300

    listbox: center text vertically

--- atflatcontrols/atlistbox.pas
+++ atflatcontrols/atlistbox.pas
@@ -612,8 +612,9 @@ procedure TATListbox.DoDefaultDrawItem(C: TCanvas; AIndex: integer; R: TRect);
 var
   Sep: TATStringSeparator;
   SLine, SItem: string;
-  NIndentLeft,
+  NIndentLeft, NIndentTop,
   NColOffset, NColWidth, NAllWidth, i: integer;
+  LineExtent: Types.TSize;
 begin
   if AIndex=FItemIndex then
   begin
@@ -639,12 +640,14 @@ begin
     SLine:= '('+IntToStr(AIndex)+')';
 
   NIndentLeft:= FIndentLeft+FIndentForX;
+  LineExtent:= C.TextExtent(SLine);
+  NIndentTop:= (FItemHeight-LineExtent.cy) div 2;
 
   if not ShowColumns then
   begin
     C.TextOut(
       R.Left+NIndentLeft-ScrollHorz,
-      R.Top+FIndentTop,
+      R.Top+NIndentTop,
       SLine);
   end
   else
@@ -667,7 +670,7 @@ begin
         );
       C.TextOut(
         NColOffset+1+IfThen(i=0, NIndentLeft),
-        R.Top+FIndentTop,
+        R.Top+NIndentTop,
         SItem
         );
 
